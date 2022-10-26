1de50e55937c62b1c3e4ccbf2f79b9e8eff31ac4
derdilla <derdilla06@gmail.com>
2022-10-26 19:48:01 +0200

    resolved merge conflicts

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,8 +1,11 @@
+import { pkg, checkValidity, createPackage } from "../spplicer/spplicer.js";
+import { VSCRIPT_BLOCKLY } from "../index.js";
+
 Neutralino.init();
 
-async function pack() {
-    // get pack information
-    // const icon = document.getElementById('iconPath').value;
+export async function pack() {
+    // regenerate code
+    VSCRIPT_BLOCKLY.updateCode();
 
     // create dlc structure
     try {await Neutralino.filesystem.getStats(`${NL_PATH}/.tmp`) }
@@ -24,28 +27,21 @@ async function pack() {
     } catch (e) {console.error(e)};
 
     // write mapspawn file
-    try { Neutralino.filesystem.writeFile(`${NL_PATH}/.tmp/portal2_dlc5/scripts/vscripts/mapspawn.nut`, mapSpawnCode) }
+    try { Neutralino.filesystem.writeFile(`${NL_PATH}/.tmp/portal2_dlc5/scripts/vscripts/mapspawn.nut`, VSCRIPT_BLOCKLY.mapSpawnCode) }
     catch (e) {console.log(e); document.getElementById('packingErrorBox').innerText = 'ERROR: check console'; return}
 
     // sanitize info for spplicer
-    updateName();
-    // pkg.name = modName.toLowerCase().replace(/ /g, "-").replace(/[^A-Za-z0-9-]/g, "");
     pkg.title = pkg.name;
     pkg.dir = `${NL_PATH}/.tmp/portal2_dlc5`;
     pkg.img =`${NL_PATH}/resources/icon.png`;
     pkg.imgext = 'png';
-    pkg.desc = modInfo.description;
+    pkg.desc = VSCRIPT_BLOCKLY.modInfo.description;
     
     checkValidity();
-    await createPackage();
-    
-    
-    console.log('done');
-    console.log(pkg);
+    createPackage();
 }
 
 Neutralino.events.on("windowClose", function () {
     Neutralino.app.exit();
   });
-
 

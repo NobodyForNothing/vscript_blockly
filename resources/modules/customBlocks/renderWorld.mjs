76ca516e3649b9f9bc31934477e05068f7bd52cd
nobodyForNothing <derdilla06@gmail.com>
2022-11-15 18:37:07 +0100

    implemented block

--- resources/modules/customBlocks/renderWorld.mjs
+++ resources/modules/customBlocks/renderWorld.mjs
@@ -2,16 +2,23 @@ import { colors } from "./customBlockDefaults.mjs";
 
 Blockly.Blocks['debug_draw_box'] = {
   init: function () {
-    this.jsonInit({
-      "type": "debug_draw_box",
-      "message0": "",
-      "args0": [
-      ],
-      "previousStatement": null,
-      "nextStatement": null,
-      "colour": colors.rendering,
-      "tooltip": "",
-      "helpUrl": ""
-    });
+    this.appendDummyInput()
+      .appendField("draw box");
+    this.appendValueInput("ORIGIN")
+      .setCheck(null)
+      .appendField("origin");
+    this.appendValueInput("SIZE")
+      .setCheck(null)
+      .appendField("size");
+    this.appendValueInput("COLOR")
+      .setCheck(null)
+      .appendField("color");
+    this.appendValueInput("OPACITY")
+      .appendField("opacity"); // todo: make field slider
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(colors.rendering);
+    this.setTooltip("Draws an overlay box in the world (DebugDrawBox)");
+    this.setHelpUrl("https://developer.valvesoftware.com/wiki/List_of_Portal_2_Script_Functions#Other");
   }
 }
